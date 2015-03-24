afd1bcc09a392859b96359621a8597d58b268da5
samiyaakhtar <samiyaakhtar7@gmail.com>
2015-03-24 13:17:37 -0400

    Code cleanup

--- android/app/src/main/java/com/uwmsa/msandbox/Activities/MainActivity.java
+++ android/app/src/main/java/com/uwmsa/msandbox/Activities/MainActivity.java
@@ -1,51 +1,15 @@
 package com.uwmsa.msandbox.Activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.Toast;
-
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
-import com.uwmsa.msandbox.Adapters.EventAdapter;
-import com.uwmsa.msandbox.Models.Event;
-import com.uwmsa.msandbox.Models.User;
+import com.uwmsa.msandbox.Models.*;
+import com.uwmsa.msandbox.Utilities.*;
 import com.uwmsa.msandbox.R;
 
-import java.util.List;
-
-public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
-
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
-
-    /**
-     * Used to display events in a list of cards
-     */
-    private RecyclerView mEventRecyclerView;
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,160 +21,14 @@ public class MainActivity extends ActionBarActivity
             if (currentUser != null) {
                 User cUser = (User) User.getCurrentUser();
                 if(cUser == null || !cUser.isAuthenticated()) {
-                    goToLoginScreen();
+                    Utilities.goToLoginScreen(this);
                 }
             }
         } catch (Exception ex) {
             Log.e("Failed: ", ex.getMessage());
         }
 
-
-        setupViews();
+        Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+        MainActivity.this.startActivity(homeIntent);
     }
-
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
-
-    protected void setupViews() {
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
-
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
-
-        // Set up the event recycler view
-        mEventRecyclerView = (RecyclerView) findViewById(R.id.main_eventRecyclerView);
-        mEventRecyclerView.setHasFixedSize(true);
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        mEventRecyclerView.setLayoutManager(llm);
-
-        fillEventsRecyclerView();
-    }
-
-
-    public void fillEventsRecyclerView() {
-        ParseQuery<Event> eventQuery = ParseQuery.getQuery(Event.CLASSNAME);
-        eventQuery.findInBackground(new FindCallback<Event>() {
-            @Override
-            public void done(List<Event> events, ParseException e) {
-                if (e != null) {
-                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                EventAdapter eventAdapter = new EventAdapter(events);
-                mEventRecyclerView.setAdapter(eventAdapter);
-            }
-        });
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-            case 4:
-                mTitle = getString(R.string.title_section4);
-                break;
-        }
-    }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_main_logout) {
-            User.logOut();
-            goToLoginScreen();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    protected void goToLoginScreen() {
-        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-        MainActivity.this.startActivity(loginIntent);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-
 }
--- android/app/src/main/java/com/uwmsa/msandbox/Adapters/EventAdapter.java
+++ android/app/src/main/java/com/uwmsa/msandbox/Adapters/EventAdapter.java
@@ -4,15 +4,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.parse.ParseFile;
 import com.parse.ParseImageView;
-import com.uwmsa.msandbox.Models.Event;
+import com.uwmsa.msandbox.Models.*;
 import com.uwmsa.msandbox.R;
-
-import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -22,20 +19,43 @@ import java.util.List;
  */
 public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventRecyclerViewHolder>  {
 
+    private OnEventClickListener mOnClickListener;
+
+    public static interface OnEventClickListener {
+        void OnEventClickListener(final Event event);
+    }
+
+    public void setOnEventClickListener(final OnEventClickListener listener) {
+        this.mOnClickListener = listener;
+    }
+
     /**
      * Created by samiya on 28/02/15.
      */
-    public static class EventRecyclerViewHolder extends RecyclerView.ViewHolder {
+    public class EventRecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         protected TextView vTitle;
         protected ParseImageView vImageView;
         protected TextView vStartTime;
+        protected TextView vCategory;
+        protected Event mEvent;
+
+        public void setEvent(Event e) {
+            this.mEvent = e;
+        }
+
+        @Override
+        public void onClick(View v) {
+            mOnClickListener.OnEventClickListener(mEvent);
+        }
 
         public EventRecyclerViewHolder(View v) {
             super(v);
             vTitle = (TextView) v.findViewById(R.id.event_title);
             vImageView = (ParseImageView) v.findViewById(R.id.event_thumbnail);
             vStartTime = (TextView) v.findViewById(R.id.event_startTime);
+            vCategory = (TextView) v.findViewById(R.id.event_category);
+            v.setOnClickListener(this);
         }
     }
 
@@ -56,12 +76,16 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventRecycle
         String eventDetails = event.getString(Event.TITLE) ;//+ " " + event.getString(Event.DESCRIPTION);
         SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, hh:mm a");
         String eventStartTime = dateFormat.format(event.getDate(Event.STARTTIME));
+        String eventCategory = event.getString(Event.CATEGORY);
         ParseFile file = event.getParseFile(Event.IMAGE);
 
         eventViewHolder.vTitle.setText(eventDetails);
         eventViewHolder.vStartTime.setText(eventStartTime);
+        eventViewHolder.vCategory.setText(eventCategory);
         eventViewHolder.vImageView.setParseFile(file);
         eventViewHolder.vImageView.loadInBackground();
+
+        eventViewHolder.setEvent(event);
     }
 
     @Override
@@ -69,11 +93,6 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventRecycle
         View itemView = LayoutInflater.
                 from(viewGroup.getContext()).
                 inflate(R.layout.event_card, viewGroup, false);
-
         return new EventRecyclerViewHolder(itemView);
     }
-
-
-
-
 }
