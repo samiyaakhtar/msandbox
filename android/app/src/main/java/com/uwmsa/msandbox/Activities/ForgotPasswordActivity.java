0c4a7d3ed7f9999ce66c1a715c6779d85fa9ec2b
Samiya <samiyaakhtar7@gmail.com>
2015-03-25 11:52:21 -0400

    Removed fragment from Event details activity. Removed unused menu xml files.

--- android/app/src/main/java/com/uwmsa/msandbox/Activities/ForgotPasswordActivity.java
+++ android/app/src/main/java/com/uwmsa/msandbox/Activities/ForgotPasswordActivity.java
@@ -28,8 +28,6 @@ public class ForgotPasswordActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_forgot_password, menu);
         return true;
     }
 
