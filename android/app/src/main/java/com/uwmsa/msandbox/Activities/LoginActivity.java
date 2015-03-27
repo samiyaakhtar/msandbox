f618fdafa025370ec39f953cc2401321d2b11833
Samiya <samiyaakhtar7@gmail.com>
2015-03-27 14:51:04 -0400

    Code cleanup on login and ui tweaks

--- android/app/src/main/java/com/uwmsa/msandbox/Activities/LoginActivity.java
+++ android/app/src/main/java/com/uwmsa/msandbox/Activities/LoginActivity.java
@@ -1,12 +1,14 @@
 package com.uwmsa.msandbox.Activities;
 
 import android.app.ProgressDialog;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,10 +24,25 @@ import com.uwmsa.msandbox.R;
 
 public class LoginActivity extends ActionBarActivity {
 
+    EditText vUsernameEditText;
+    EditText vPasswordEditText;
+    Button vLoginButton;
+    Button vSignupButton;
+    TextView vForgotPasswordButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        vUsernameEditText = (EditText)findViewById(R.id.login_usernameTextField);
+        vPasswordEditText = (EditText)findViewById(R.id.login_passwordTextField);
+        vLoginButton = (Button) findViewById(R.id.login_loginButton);
+        vSignupButton = (Button) findViewById(R.id.login_signUpButton);
+        vForgotPasswordButton = (TextView) findViewById(R.id.login_forgotPasswordButton);
+
+        vPasswordEditText.setTypeface(Typeface.DEFAULT);
+        vPasswordEditText.setTransformationMethod(new PasswordTransformationMethod());
 
         setupButtonListeners();
 
@@ -41,32 +58,18 @@ public class LoginActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-
         return super.onOptionsItemSelected(item);
     }
 
     protected void setupButtonListeners() {
-        Button loginButton = (Button) findViewById(R.id.login_loginButton);
-        loginButton.setOnClickListener(new Button.OnClickListener() {
+        vLoginButton.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
 
                 final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
                 progressDialog.setMessage("Logging In");
                 progressDialog.show();
 
-                EditText usernameTextField = (EditText)findViewById(R.id.login_usernameTextField);
-                EditText passwordTextField = (EditText)findViewById(R.id.login_passwordTextField);
-
-                ParseUser.logInInBackground(usernameTextField.getText().toString(), passwordTextField.getText().toString(), new LogInCallback() {
+                ParseUser.logInInBackground(vUsernameEditText.getText().toString(), vPasswordEditText.getText().toString(), new LogInCallback() {
                     @Override
                     public void done(ParseUser parseUser, ParseException e) {
                         progressDialog.dismiss();
@@ -86,16 +89,14 @@ public class LoginActivity extends ActionBarActivity {
             }
         });
 
-        Button signUpButton = (Button) findViewById(R.id.login_signUpButton);
-        signUpButton.setOnClickListener(new Button.OnClickListener() {
+        vSignupButton.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 Intent loginIntent = new Intent(LoginActivity.this, SignupActivity.class);
                 LoginActivity.this.startActivity(loginIntent);
             }
         });
 
-        TextView forgotPasswordButton = (TextView) findViewById(R.id.login_forgotPasswordButton);
-        forgotPasswordButton.setOnClickListener(new View.OnClickListener() {
+        vForgotPasswordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent loginIntent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
