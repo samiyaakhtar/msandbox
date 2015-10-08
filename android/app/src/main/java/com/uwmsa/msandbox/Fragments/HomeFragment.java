4bc85cc46d16bf819b7aad1b4b8f7693dc16e4cf
Mohamed <mmmoussa@uwaterloo.ca>
2015-10-08 19:37:49 -0400

    Implemented retrieval of prayer times from parse based on today's date

--- android/app/src/main/java/com/uwmsa/msandbox/Fragments/HomeFragment.java
+++ android/app/src/main/java/com/uwmsa/msandbox/Fragments/HomeFragment.java
@@ -5,15 +5,28 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.uwmsa.msandbox.Activities.MainActivity;
 import com.uwmsa.msandbox.Adapters.PrayerLocationMainAdapter;
 import com.uwmsa.msandbox.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
 
 import io.karim.MaterialTabs;
 
@@ -26,6 +39,7 @@ public class HomeFragment extends Fragment {
     private TextView asrTime;
     private TextView maghribTime;
     private TextView ishaTime;
+    private ParseObject prayerTime;
 
     public static HomeFragment newInstance(int sectionNumber) {
         HomeFragment fragment = new HomeFragment();
@@ -50,13 +64,34 @@ public class HomeFragment extends Fragment {
         maghribTime = (TextView) homeView.findViewById(R.id.maghribTime);
         ishaTime = (TextView) homeView.findViewById(R.id.ishaTime);
 
-        // ParseQuery query = new ParseQuery();
+        Calendar c = Calendar.getInstance();
 
-        fajrTime.setText("6:03");
-        dhuhrTime.setText("8:03");
-        asrTime.setText("10:03");
-        maghribTime.setText("2:03");
-        ishaTime.setText("8:03");
+        SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        final String formattedDate = df.format(c.getTime());
+
+        ParseQuery query = new ParseQuery("PrayerTimes");
+        query.whereEqualTo("date", formattedDate);
+        query.findInBackground(new FindCallback() {
+            @Override
+            public void done(List list, ParseException e) {
+                if (e == null) {
+                    if (list.size() > 0) {
+                        prayerTime = (ParseObject) list.get(0);
+                        HashMap eqamaTime = (HashMap) prayerTime.get("eqama");
+                        fajrTime.setText(eqamaTime.get("fajr").toString());
+                        List<String> dhuhrList = Arrays.asList(eqamaTime.get("dhuhr").toString().split(","));
+                        dhuhrTime.setText(dhuhrList.get(0));
+                        asrTime.setText(eqamaTime.get("asr").toString());
+                        maghribTime.setText(eqamaTime.get("maghrib").toString());
+                        ishaTime.setText(eqamaTime.get("isha").toString());
+                    } else {
+                        System.out.println("No appropriate results for " + formattedDate);
+                    }
+                } else {
+                    Log.e("Prayer times failed: ", e.getMessage());
+                }
+            }
+        });
 
         return homeView;
     }
