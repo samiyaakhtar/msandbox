2e66db96163e2ec086df721667a5335288601622
Zunair <zunair_syed@hotmail.com>
2015-11-01 13:54:35 -0500

    files git

--- android/app/src/main/java/com/uwmsa/msandbox/Activities/EventDetailsActivity.java
+++ android/app/src/main/java/com/uwmsa/msandbox/Activities/EventDetailsActivity.java
@@ -1,17 +1,21 @@
 package com.uwmsa.msandbox.Activities;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.GetCallback;
 import com.parse.ParseException;
@@ -20,21 +24,35 @@ import com.uwmsa.msandbox.Models.Event;
 import com.uwmsa.msandbox.R;
 import com.uwmsa.msandbox.Utilities.Utilities;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+
 public class EventDetailsActivity extends ActionBarActivity {
 
+    private Context context;
     private Event mEvent;
     private ParseImageView vImageView;
-    private TextView vDescription;
-    private TextView vCategory;
-    private TextView vFacebookEvent;
     private TextView vLocation;
-    private TextView vStartTime;
-    private TextView vEndTime;
+    private TextView vPrice;
+    private TextView vStartEndTime;
+    private TextView vDescription;
+    private TextView vTitle;
+
+    private ScrollView scrollView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+        context = this;
+
+        vImageView = (ParseImageView) findViewById(R.id.eventDetails_thumbnail);
+        vLocation = (TextView) findViewById(R.id.ed_location_venue_tv);
+        vPrice = (TextView) findViewById(R.id.ed_price_tv);
+        vDescription = (TextView) findViewById(R.id.ed_more_info_tv);
+        vStartEndTime = (TextView) findViewById(R.id.ed_start_end_tv);
+        vTitle = (TextView) findViewById(R.id.ed_title_tv);
 
         String eventObjectId = getIntent().getStringExtra("eventObjectId");
         Event.fetchEventInBackground(eventObjectId, new GetCallback<Event>() {
@@ -45,46 +63,75 @@ public class EventDetailsActivity extends ActionBarActivity {
                     fillEventDetailsView();
                 } else {
                     // TODO: handle event error
+                    Toast.makeText(context, "Oops, something went wrong ", Toast.LENGTH_LONG).show();
                 }
             }
         });
 
-        vImageView = (ParseImageView) findViewById(R.id.eventDetails_thumbnail);
-        vDescription = (TextView) findViewById(R.id.eventDetails_description);
-        vLocation = (TextView) findViewById(R.id.eventDetails_location);
-        vStartTime = (TextView) findViewById(R.id.eventDetails_startTime);
-        vEndTime = (TextView) findViewById(R.id.eventDetails_endTime);
+        toolbar = (Toolbar) findViewById(R.id.transparent_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setPadding(0, getStatusBarHeight(), 0, 0);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.eventdetails_menu, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        if (id == android.R.id.home) {
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 
     protected void fillEventDetailsView() {
         vDescription.setText(mEvent.getDescription());
+        vTitle.setText(mEvent.getTitle());
+        vLocation.setText(mEvent.getLocation());
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        vPrice.setText("$"+df.format(mEvent.getTicketPrice().doubleValue()));
+
         vImageView.setParseFile(mEvent.getImage());
         vImageView.loadInBackground();
-        vStartTime.setText(Html.fromHtml("<b>Starts</b>: " + Utilities.getStringFromDate(mEvent.getStartTime())));
-        vEndTime.setText(Html.fromHtml("<b>Ends</b>: " + Utilities.getStringFromDate(mEvent.getEndTime())));
-        vLocation.setText(Html.fromHtml("<b>Location</b>: " + mEvent.getLocation()));
-        setTitle(mEvent.getTitle());
+
+        vStartEndTime.setText(getFormattedStartEndDate());
     }
+
+
+    private String getFormattedStartEndDate(){
+        String date;
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd");
+        date = dateFormat.format(mEvent.getStartTime());
+        date += " - " + dateFormat.format(mEvent.getEndTime()) + ", ";
+
+        dateFormat = new SimpleDateFormat("hh:mm");
+        date += dateFormat.format(mEvent.getStartTime()) + " - ";
+
+        dateFormat = new SimpleDateFormat("hh:mm a");
+        date += dateFormat.format(mEvent.getStartTime());
+
+        return date;
+    }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == android.R.id.home) {
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+
+
 }
