9fa80472d77249c4b45a803bfd9520e313ceb670
Mohamed <mmmoussa@uwaterloo.ca>
2015-11-19 22:50:51 -0500

    Added sections to list of prayer locations

--- android/app/src/main/java/com/uwmsa/msandbox/Fragments/PrayerLocationDailyFragment.java
+++ android/app/src/main/java/com/uwmsa/msandbox/Fragments/PrayerLocationDailyFragment.java
@@ -1,6 +1,7 @@
 package com.uwmsa.msandbox.Fragments;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -19,9 +20,12 @@ import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.uwmsa.msandbox.Activities.MainActivity;
 import com.uwmsa.msandbox.Adapters.PrayerLocationDailyAdapter;
+import com.uwmsa.msandbox.Adapters.PrayerSectionedRecyclerViewAdapter;
 import com.uwmsa.msandbox.Models.PrayerRoomLocation;
 import com.uwmsa.msandbox.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import io.karim.MaterialTabs;
@@ -36,6 +40,7 @@ public class PrayerLocationDailyFragment extends Fragment implements SwipeRefres
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private MaterialTabs tabs;
     boolean constructorCalled;
+    private Context context;
 
     public PrayerLocationDailyFragment() {
         constructorCalled = true;
@@ -44,6 +49,8 @@ public class PrayerLocationDailyFragment extends Fragment implements SwipeRefres
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        context = getActivity();
 
         View rootView = inflater.inflate(R.layout.fragment_prayer_location_daily, container, false);
         mSwipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.refreshDaily);
@@ -65,16 +72,45 @@ public class PrayerLocationDailyFragment extends Fragment implements SwipeRefres
     }
 
     private void getLocations() {
+        String[] prayerTypes = {"Daily", "Jumuah"};
         ParseQuery<PrayerRoomLocation> query = ParseQuery.getQuery(PrayerRoomLocation.class);
-        query.whereEqualTo(PrayerRoomLocation.TYPE, "Daily");
+        query.whereContainedIn(PrayerRoomLocation.TYPE, Arrays.asList(prayerTypes));
         query.setCachePolicy(ParseQuery.CachePolicy.NETWORK_ELSE_CACHE);
         query.findInBackground(new FindCallback<PrayerRoomLocation>() {
             @Override
             public void done(List<PrayerRoomLocation> prayerRoomLocations, ParseException e) {
                 if (e == null) {
-                    PrayerLocationDailyAdapter adapter = new PrayerLocationDailyAdapter(prayerRoomLocations, mSwipeRefreshLayout.isRefreshing() || constructorCalled);
+                    List<PrayerRoomLocation> dailyPrayerRoomLocations = new ArrayList<>();
+                    List<PrayerRoomLocation> jumuahPrayerRoomLocations = new ArrayList<>();
+                    for (int i = 0; i < prayerRoomLocations.size(); i++) {
+                        if (prayerRoomLocations.get(i).getType().equals("Daily")) {
+                            dailyPrayerRoomLocations.add(prayerRoomLocations.get(i));
+                        } else if (prayerRoomLocations.get(i).getType().equals("Jumuah")) {
+                            jumuahPrayerRoomLocations.add(prayerRoomLocations.get(i));
+                        }
+                    }
+
+                    List<PrayerRoomLocation> orderedPrayerRoomLocations = new ArrayList<>(dailyPrayerRoomLocations);
+                    orderedPrayerRoomLocations.addAll(jumuahPrayerRoomLocations);
+
+                    PrayerLocationDailyAdapter adapter = new PrayerLocationDailyAdapter(orderedPrayerRoomLocations, mSwipeRefreshLayout.isRefreshing() || constructorCalled);
                     constructorCalled = false;
-                    mPrayerRoomRecyclerView.setAdapter(adapter);
+
+                    //This is the code to provide a sectioned list
+                    List<PrayerSectionedRecyclerViewAdapter.Section> sections =
+                            new ArrayList<PrayerSectionedRecyclerViewAdapter.Section>();
+
+                    //Sections
+                    sections.add(new PrayerSectionedRecyclerViewAdapter.Section(0, "Daily Prayers"));
+                    sections.add(new PrayerSectionedRecyclerViewAdapter.Section(dailyPrayerRoomLocations.size(), "Jumuah Prayers"));
+
+                    //Add your adapter to the sectionAdapter
+                    PrayerSectionedRecyclerViewAdapter.Section[] dummy = new PrayerSectionedRecyclerViewAdapter.Section[sections.size()];
+                    PrayerSectionedRecyclerViewAdapter mSectionedAdapter = new
+                            PrayerSectionedRecyclerViewAdapter(context,R.layout.prayer_section_title,R.id.section_text,adapter);
+                    mSectionedAdapter.setSections(sections.toArray(dummy));
+
+                    mPrayerRoomRecyclerView.setAdapter(mSectionedAdapter);
                     if(mSwipeRefreshLayout.isRefreshing())
                         mSwipeRefreshLayout.setRefreshing(false);
                 } else {
