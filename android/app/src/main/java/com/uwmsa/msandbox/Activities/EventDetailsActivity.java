4c27c0ccec0ddaa69ba93a50778aa50ef17c3adf
Zunair <zunair_syed@hotmail.com>
2015-11-20 00:27:15 -0500

    Added Features and Fixes for various things in event pages

--- android/app/src/main/java/com/uwmsa/msandbox/Activities/EventDetailsActivity.java
+++ android/app/src/main/java/com/uwmsa/msandbox/Activities/EventDetailsActivity.java
@@ -1,11 +1,23 @@
 package com.uwmsa.msandbox.Activities;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.ShareCompat;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.OnNavigationListener;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.ShareCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.util.Log;
@@ -13,10 +25,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.flipboard.bottomsheet.BottomSheetLayout;
+import com.flipboard.bottomsheet.commons.IntentPickerSheetView;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseImageView;
@@ -26,6 +42,8 @@ import com.uwmsa.msandbox.Utilities.Utilities;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class EventDetailsActivity extends ActionBarActivity {
 
@@ -40,12 +58,16 @@ public class EventDetailsActivity extends ActionBarActivity {
 
     private ScrollView scrollView;
     private Toolbar toolbar;
+    ShareActionProvider shareActionProvider;
+    BottomSheetLayout bottomSheetLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
         context = this;
+
+        bottomSheetLayout = (BottomSheetLayout) findViewById(R.id.container);
 
         vImageView = (ParseImageView) findViewById(R.id.eventDetails_thumbnail);
         vLocation = (TextView) findViewById(R.id.ed_location_venue_tv);
@@ -81,9 +103,53 @@ public class EventDetailsActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.eventdetails_menu, menu);
-        return true;
+
+//
+//        // Create the share Intent
+//        String playStoreLink = "https://play.google.com/store/apps/details?id=" + getPackageName();
+//        String yourShareText = "Test Share Link" + playStoreLink;
+//        Intent shareIntent = ShareCompat.IntentBuilder.from(this).setType("text/plain").setText(yourShareText).getIntent();
+//
+//
+//        MenuItem menuItem = menu.findItem(R.id.ed_action_share);
+//        shareActionProvider = new ShareActionProvider(this);
+//        shareActionProvider.setShareIntent(shareIntent);
+//        MenuItemCompat.setActionProvider(menuItem, shareActionProvider);
+
+        return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+//        Toast.makeText(context, "SOME SELECTED", Toast.LENGTH_LONG).show();
+//        if(item.getItemId() == R.id.ed_action_share){
+//            Toast.makeText(context, "SHARE SELECTED", Toast.LENGTH_LONG).show();
+//            onShareAction();
+//        }
+
+        Toast.makeText(context, "SOME SELECTED", Toast.LENGTH_LONG).show();
+        if(item.getItemId() == R.id.ed_action_share){
+            Toast.makeText(context, "SHARE SELECTED", Toast.LENGTH_LONG).show();
+            bottomSheetShareAction();
+        }
+
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onShareAction(){
+        // Create the share Intent
+        String playStoreLink = "https://play.google.com/store/apps/details?id=" + getPackageName();
+        String yourShareText = "Test Share Link" + playStoreLink;
+        Intent shareIntent = ShareCompat.IntentBuilder.from(this).setType("text/plain").setText(yourShareText).getIntent();
+        // Set the share Intent
+        if (shareActionProvider != null) {
+            shareActionProvider.setShareIntent(shareIntent);
+        }
+    }
 
     protected void fillEventDetailsView() {
         vDescription.setText(mEvent.getDescription());
@@ -125,12 +191,47 @@ public class EventDetailsActivity extends ActionBarActivity {
         return result;
     }
 
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem menuItem) {
-        if (menuItem.getItemId() == android.R.id.home) {
-        }
-        return super.onOptionsItemSelected(menuItem);
+    public void onBackPressed() {
+        MainActivity.comingFromEventDetail =true;
+        super.onBackPressed();
     }
 
 
+
+    private void bottomSheetShareAction(){
+
+        final Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "This is the MSA APP");
+        shareIntent.setType("text/plain");
+        IntentPickerSheetView intentPickerSheet = new IntentPickerSheetView(EventDetailsActivity.this, shareIntent, "Share with...", new IntentPickerSheetView.OnIntentPickedListener() {
+            @Override
+            public void onIntentPicked(IntentPickerSheetView.ActivityInfo activityInfo) {
+                bottomSheetLayout.dismissSheet();
+                startActivity(activityInfo.getConcreteIntent(shareIntent));
+            }
+        });
+        // Filter out built in sharing options such as bluetooth and beam.
+        intentPickerSheet.setFilter(new IntentPickerSheetView.Filter() {
+            @Override
+            public boolean include(IntentPickerSheetView.ActivityInfo info) {
+                return !info.componentName.getPackageName().startsWith("com.android");
+            }
+        });
+        // Sort activities in reverse order for no good reason
+        intentPickerSheet.setSortMethod(new Comparator<IntentPickerSheetView.ActivityInfo>() {
+            @Override
+            public int compare(IntentPickerSheetView.ActivityInfo lhs, IntentPickerSheetView.ActivityInfo rhs) {
+                return rhs.label.compareTo(lhs.label);
+            }
+        });
+
+        // Add custom mixin example
+//        Drawable customDrawable = ResourcesCompat.getDrawable(getResources(), R.mipmap.ic_launcher, null);
+//        IntentPickerSheetView.ActivityInfo customInfo = new IntentPickerSheetView.ActivityInfo(customDrawable, "Custom mix-in", EventDetailsActivity.this, MainActivity.class);
+//        intentPickerSheet.setMixins(Collections.singletonList(customInfo));
+
+        bottomSheetLayout.showWithSheetView(intentPickerSheet);
+    }
 }
